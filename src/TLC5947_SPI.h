393cbaa52bcdc006825aea511cf343821735de41
Tom Broad <tom+github@uberhq.co.uk>
2019-08-04 22:14:38 +0100

    Updated licence

--- src/TLC5947_SPI.h
+++ src/TLC5947_SPI.h
@@ -1,3 +1,21 @@
+/***************************************************
+  This is an example for our Adafruit 24-channel PWM/LED driver
+  Pick one up today in the adafruit shop!
+
+  ------> http://www.adafruit.com/products/1429
+
+  These drivers uses SPI to communicate, 3 pins are required to
+  interface: Data, Clock and Latch. The boards are chainable
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+
+  Modified to use hardware SPI by Tom Broad (tcbroad@gmail.com)
+ ****************************************************/
 //
 // Created by tombr on 24/07/2019.
 //
